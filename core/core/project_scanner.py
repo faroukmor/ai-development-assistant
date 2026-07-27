22be50d23cd2fe23d979422a52c5a18d3d5b59d2
faroukmor <faroukmorsli2@gmail.com>
2026-07-27 14:43:04 +0100

    add project scanner

---
+++ core/core/project_scanner.py
@@ -0,0 +1,58 @@
+import os
+import projectFile as pf
+path = "C:\\Users\\HP\\Documents\\PYTHON Project\\ai-development-assistant"
+
+IGNORED = {".git", "__pycache__", "venv", ".idea", ".vscode", ".gitignore"}
+def scan_project(path):
+    files_list = []
+    for f in os.listdir(path):
+        if f in IGNORED or f.startswith("."): 
+            continue
+
+        full_path = os.path.join(path, f)
+        if os.path.isdir(full_path):
+            files_list.append(full_path+ os.sep)
+            files_list.extend(scan_project(full_path))
+        else:
+            files_list.append(full_path)
+            
+    return files_list
+
+
+def scan_project_obj(path):
+    files_obj = []
+    files_list = scan_project(path)
+    
+    for file in files_list:
+        files_obj.append(pf.ProjectFile(file))
+        
+    return files_obj
+
+"""def get_files_path(path):
+    files_list = []
+    files_obj = []
+    for f in os.listdir(path):
+        if f in IGNORED or f.startswith("."): 
+            continue
+
+        full_path = os.path.join(path, f)
+        if os.path.isdir(full_path):
+            files_list.extend(scan_project(full_path))
+        else:
+            files_list.append(full_path)
+    
+
+    return files_files"""
+
+
+
+
+file_obj = scan_project_obj(path)
+for file in file_obj:
+    print(file.path)
+    print(file.name)
+    print(file.ext)
+    print(file.size)
+    print(file.programingLanguage)
+    print(file.last_time_edited)
+    
