27aa9ea6e560570e66b5a44508fa596780296854
faroukmor <faroukmorsli2@gmail.com>
2026-08-27 23:10:29 +0100

    Improve project context with symbol calls

--- core/context/symbol_context_builder.py
+++ core/context/symbol_context_builder.py
@@ -1,7 +1,9 @@
+import core.context.symbol_dependency_builder as SDB
 class SymbolContextBuilder:
     def __init__(self,project,retrievers):
         self.project = project
         self.retrievers = retrievers
+        self.dependency_builder = SDB.SymbolDependencyBuilder(self.project)
     
 
     def get_symbol_source(self, symbol, file):
@@ -22,22 +24,32 @@ class SymbolContextBuilder:
     def format_symbol(self, symbol, file, score=None, reason=None):
         symbol_source = self.get_symbol_source(symbol, file)
         symbol_calls = self.get_symbol_calls(symbol)
-        return f"""
-    File: {file.name}
-    Relevance: {score if score is not None else ""}
-    Reason: {reason if reason else ""}
-    
-    Symbol:
-    Name: {symbol.name}
-    Type: {symbol.type}
-    Signature: {symbol.signature}
-    Parent: {symbol.parent.name if symbol.parent else ""}
-    Docstring: {symbol.docstring}
-    Source:
-    {symbol_source}
-    calls:
-    {symbol_calls}
-    """
+        called_symbols = ""
+        results = self.dependency_builder.find_call_symbols(symbol)
+        for result in results:
+            called_symbols += f"""
+called Symbol: {result.symbol.name}
+Type: {result.symbol.type}
+File: {result.file.name}
+Signature: {result.symbol.signature}
+Source: {self.get_symbol_source(result.symbol,result.file)}"""
+        return f"""File: {file.name}
+Relevance: {score if score is not None else ""}
+Reason: {reason if reason else ""}
+
+Symbol:
+Name: {symbol.name}
+Type: {symbol.type}
+Signature: {symbol.signature}
+Parent: {symbol.parent.name if symbol.parent else ""}
+Docstring: {symbol.docstring}
+Source:
+{symbol_source}
+calls:
+{symbol_calls}
+Called Symbols:
+{called_symbols}
+"""
 
     def build_symbol_context(self):
         symbol_context = ""
--- core/context/symbol_dependency_builder.py
+++ core/context/symbol_dependency_builder.py
@@ -3,3 +3,10 @@ class SymbolDependencyBuilder:
     def __init__(self,project):
         self.project = project
         self.symbol_search = SS.SymbolSearch(project)
+
+    def find_call_symbols(self, symbol):
+        search_results = []
+        for call in symbol.calls:
+            call = call.replace("()", "")
+            search_results += self.symbol_search.find_by_name(call)
+        return search_results
